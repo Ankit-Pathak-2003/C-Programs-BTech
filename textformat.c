1e225bfabb20312e7e97fe37e12585c8ac15dfbc
Ankit Pathak <92701127+Ankit-Pathak-2003@users.noreply.github.com>
2021-11-16 12:42:45 +0530

    Add files via upload

---
+++ textformat.c
@@ -0,0 +1,8 @@
+/*write a program to format console output using \n,\t,\b within printf statement*/
+#include<stdio.h>
+int main(){
+	printf("\t this statement is tabbed\n");
+	printf("\n this statement is a new line\n");
+	printf("There is a backslashed \bHi used");
+	return 0;
+}
